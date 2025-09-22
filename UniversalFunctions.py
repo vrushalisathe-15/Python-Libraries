020ae387f40bc311479696769c2f6577e66d6e63
Vrushali Sathe <vrushalisathe95@gmail.com>
2025-09-22 16:19:25 +0530

    Add files via upload

---
+++ UniversalFunctions.py
@@ -0,0 +1,24 @@
+import numpy as np
+arr=np.array([81,100,4,225,16])
+
+#square root
+print(np.sqrt(arr))
+
+#exponential
+print(np.exp([1,2,3]))
+
+#log
+print(np.log([1,2,3]))
+
+#sin
+print(np.sin(arr))
+
+#cos
+print(np.cos(arr))
+
+#absolute
+arr2=np.array([-1,3,6,-9])
+print(np.absolute(arr2))
+
+#log2
+print(np.log2(arr))
