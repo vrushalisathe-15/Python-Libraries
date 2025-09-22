c75dbae3a05132d638191c003884d4a3d1651960
Vrushali Sathe <vrushalisathe95@gmail.com>
2025-09-22 16:17:22 +0530

    Add files via upload

---
+++ ArithmeticOperations.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#Arithmetic operations on array
+arr1=np.array([4,5,6])
+arr2=np.array([9,8,7])
+#ADDITION
+print("Addition of an array is" , arr1+arr2)
+#SUBTRACTION
+print("Subtraction of an array is:", arr1-arr2)
+
+print("Multiplication of an array is:", arr1*arr2)
+
+print("Division of an array is:", arr1/arr2)
+
+print("Modulus of an array is:", arr1%arr2)
+
+print("Exponentiation of an array is:", arr1**arr2)
+
+print("Floor division of an array is:", arr1/arr2)
+
